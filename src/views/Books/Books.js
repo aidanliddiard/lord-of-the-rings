d10ff5aedee34970b3d73661b18d16152ddf53cb
aidanliddiard <aidanliddiard@gmail.com>
2022-03-21 17:09:07 -0700

    books displaying on the page

--- src/views/Books/Books.js
+++ src/views/Books/Books.js
@@ -1,14 +1,26 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { fetchBooks } from '../../services/books';
 
 export default function Books() {
+  const [books, setBooks] = useState([]);
+
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchBooks();
       console.log(data);
+      setBooks(data);
     };
     fetchData();
   }, []);
 
-  return <div>Books</div>;
+  return (
+    <div>
+      <h3>Books</h3>
+      {books.map((book) => (
+        <div key={book.id}>
+          <h5>{book.title}</h5>
+        </div>
+      ))}
+    </div>
+  );
 }
